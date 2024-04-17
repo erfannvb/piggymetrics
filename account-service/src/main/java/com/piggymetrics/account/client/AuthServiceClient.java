faf865793321032afb2b2dfae1657d5244d50ca3
erfannvb <erfannavab@gmail.com>
2024-04-17 10:56:35 +0330

    replace @RequestMapping with @PostMapping

--- account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
+++ account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
@@ -3,13 +3,12 @@ package com.piggymetrics.account.client;
 import com.piggymetrics.account.domain.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "auth-service")
 public interface AuthServiceClient {
 
-	@RequestMapping(method = RequestMethod.POST, value = "/uaa/users", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	void createUser(User user);
+    @PostMapping(path = "/uaa/users", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    void createUser(User user);
 
 }
